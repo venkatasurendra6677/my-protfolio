d2bfebdbea92b8870d46031cf20f2f531e991735
venkatasurendra6677 <venkatasurendra6677@gmail.com>
2025-10-17 21:41:17 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-document.addEventListener('DOMContentLoaded', function() {
     // Mobile menu toggle
     const hamburger = document.querySelector('.hamburger');
     const navLinks = document.querySelector('.nav-links');
